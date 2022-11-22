932427924806bdfcb0f841b88e68393c0ee3ccc8
Nowsath04 <110928124+Nowsath04@users.noreply.github.com>
2022-11-22 20:00:18 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter,Routes,Route,} from "react-router-dom";
+import Dashboard from "./components/dashboard/dasboard"
+import Login from "./components/login/login.js"
+import Demo from "./demo.js";
+import Signup from "./components/signup/signup.js"
+import Servicereq from "./components/servicereq/servicereq.js"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+  
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/signup" element={<Signup />}></Route> 
+      <Route path="/demo" element={<Demo />}></Route> 
+      <Route path="/dashboard" element={<Dashboard />}></Route> 
+      <Route path="/servicereq" element={<Servicereq />}></Route> 
+      {/* <Route path="/profile" element={<Profile />}></Route>  */}
+    </Routes>
+  </BrowserRouter>
+   
+    
+    </>
+    
   );
 }
 
